47bebc7fa87b72b93ee4b0c7bb963458ad59c8db
QirongChen0910 <chen0910@algonquinlive.com>
2024-07-16 15:51:41 -0400

    4 button and four page - 07-16-1 -qirong

---
+++ lib/AirplaneListPage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AirplaneListPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Airplane List Page'),
+      ),
+      body: Center(
+        child: const Text('This is the Airplane List Page'),
+      ),
+    );
+  }
+}
---
+++ lib/CustomerListPage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CustomerListPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Customer List Page'),
+      ),
+      body: Center(
+        child: const Text('This is the Customer List Page'),
+      ),
+    );
+  }
+}
